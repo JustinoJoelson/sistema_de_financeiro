009ff354a47f30814380dcb4412334543ef33972
Joelson Justino <joelsonjustino58@gmail.com>
2023-07-17 23:45:24 -0300

    processo

--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from perfil.models import Conta
+from perfil.models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
  
@@ -9,7 +9,11 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
-    return render(request, 'gerenciar.html', {'contas': contas})
+    categorias = Categoria.objects.all()
+    total_contas = 0
+    for conta in contas:
+        total_contas += conta.valor
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -32,7 +36,34 @@ def cadastrar_banco(request):
     )
 
     conta.save() 
-    messages.add_message(request, constants.SUCCESS, 'Conta Cadastrada com sucesso')
+    messages.add_message(request, constants.SUCCESS, 'Conta Cadastrada com sucesso!')
+    return redirect('/perfil/gerenciar/')
     
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+    messages.add_message(request, constants.SUCCESS, 'Conta Deletada Com Sucesso!')
+
     return redirect('/perfil/gerenciar/')
 
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
+    return redirect('/perfil/gerenciar/')
