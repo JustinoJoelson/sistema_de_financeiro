dfc09e5b7b075a230836e4d68e366a714e38989b
Joelson Justino <joelsonjustino58@gmail.com>
2023-07-18 17:21:31 -0300

    validações

--- perfil/views.py
+++ perfil/views.py
@@ -5,7 +5,11 @@ from django.contrib import messages
 from django.contrib.messages import constants
  
 def home(request):
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+    total_contas = 0
+    for conta in contas:
+        total_contas += conta.valor
+    return render(request, 'home.html', {'contas': contas, 'total_contas': total_contas})
 
 def gerenciar(request):
     contas = Conta.objects.all()
@@ -22,7 +26,7 @@ def cadastrar_banco(request):
     valor = request.POST.get('valor')
     icone = request.FILES.get('icone')
     
-    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0 or not icone:
         messages.add_message(request, constants.ERROR, 'preencha todos os campos ')
         return redirect('/perfil/gerenciar/')
 
